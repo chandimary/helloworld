766ee4a88f5164725319ef8e0d5113c8af17e85b
chandimary <chandimarygloria@gmail.com>
2019-11-05 09:51:54 +0300

    more

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+//console.log('hello world');
+/*creating codes for a hello world app to be displayed on the browser
+reguire is a key word used for accessing a package of what we need to help us
+show our code on the browser
+*/
+const express = require('express')
+const app = express();
+app.listen(3000, function(){
+    console.log('listening on 3000')
+})
+//app.listen method to create so that sever can listen to the port 3000
+//NOTE:browsers always send GET requests to the severs so that they can READ
+//in express we handle the Get requests with the get method
+//app.get(path,callback)//this is how they write the get method
+
+app.get('/', function(req, res){
+    res.send('Hello World');
+})
+
